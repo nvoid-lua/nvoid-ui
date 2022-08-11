b449f320d17143cb4f053943a130415184206240
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-11 14:49:53 +0200

    some stuff

---
+++ lua/config.lua
@@ -0,0 +1,7 @@
+local opt = {
+  statusline = {
+    enable = true,
+    style = "nvoid"
+  }
+}
+return opt
--- lua/ui/statusline/init.lua
+++ lua/ui/statusline/init.lua
@@ -1,51 +1,60 @@
 local S = {}
 
-local config = require("nvoid.core.utils").load_config().ui.statusline
+local StyleD = require("config").statusline.style
+local StyleN = require("nvoid.core.utils").load_config().ui.statusline.style
+local Style = StyleN or StyleD
+
+local configD = require("config").statusline.enable
+local configN = require("nvoid.core.utils").load_config().ui.statusline.enable
+local config = configN or configD
+
 local M = require "ui.statusline.modules"
 
 S.run = function()
-  if config == "nvoid" then
-    return table.concat {
-      M.modeN(),
-      M.fileInfo(),
-      M.git(),
+  if config then
+    if Style == "nvoid" then
+      return table.concat {
+        M.modeN(),
+        M.fileInfo(),
+        M.git(),
 
-      "%=",
-      M.lsp_progress(),
-      "%=",
+        "%=",
+        M.lsp_progress(),
+        "%=",
 
-      M.diagnostics(),
-      M.lsp(),
-      M.scrollbar(),
-    }
-  elseif config == "minimal" then
-    return table.concat {
-      M.modeM(),
-      M.git(),
-      " ",
-      M.diagnostics(),
+        M.diagnostics(),
+        M.lsp(),
+        M.scrollbar(),
+      }
+    elseif Style == "minimal" then
+      return table.concat {
+        M.modeM(),
+        M.git(),
+        " ",
+        M.diagnostics(),
 
-      "%=",
-      M.lsp_progress(),
-      "%=",
+        "%=",
+        M.lsp_progress(),
+        "%=",
 
-      M.fileInfo(),
-      M.scrollbar(),
-    }
-  elseif config == "evil" then
-    return table.concat {
-      M.modeE(),
-      M.fileInfo(),
-      M.git(),
+        M.fileInfo(),
+        M.scrollbar(),
+      }
+    elseif Style == "evil" then
+      return table.concat {
+        M.modeE(),
+        M.fileInfo(),
+        M.git(),
 
-      "%=",
-      M.lsp_progress(),
-      "%=",
+        "%=",
+        M.lsp_progress(),
+        "%=",
 
-      M.diagnostics(),
-      M.lsp(),
-      M.scrollbar(),
-    }
+        M.diagnostics(),
+        M.lsp(),
+        M.scrollbar(),
+      }
+    end
   end
 end
 
