432f9535ed83bd03b1c7c4d5e20039052e740bb3
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-14 01:29:55 +0200

    Better Updater

--- lua/ui.lua
+++ lua/ui.lua
@@ -1,6 +1,31 @@
 local M = {}
 local api = vim.api
 
+M.echo = function(opts)
+   if opts == nil or type(opts) ~= "table" then
+      return
+   end
+   vim.api.nvim_echo(opts, false, {})
+end
+M.clear_last_echo = function()
+   -- wrap this with inputsave and inputrestore just in case
+   vim.fn.inputsave()
+   vim.api.nvim_feedkeys(":", "nx", true)
+   vim.fn.inputrestore()
+end
+M.cmd = function(cmd, print_error)
+   local result = vim.fn.system(cmd)
+   if vim.api.nvim_get_vvar "shell_error" ~= 0 then
+      if print_error then
+         vim.api.nvim_err_writeln("Error running command:\n" .. cmd .. "\nError message:\n" .. result)
+      end
+      return nil
+   end
+   return result
+end
+
+M.update_nvoid = require "updater.update"
+
 -- BufferLine
 M.bufferclose = function(bufnr)
   if vim.bo.buftype == "terminal" then
@@ -63,7 +88,7 @@ M.closeallbufs = function(action)
   end
 end
 
-M.cmd = function()
+M.cmds = function()
   local new_cmd = vim.api.nvim_create_user_command
   -- Rename
   new_cmd("NvoidRename", function()
@@ -101,7 +126,7 @@ end
 
 function M.setup(options)
   require("base16").load_theme()
-  require("ui").cmd()
+  require("ui").cmds()
   vim.opt.statusline = "%!v:lua.require'ui.statusline'.run()"
   vim.opt.tabline = "%!v:lua.require'ui.bufferline'.run()"
   require("config").setup(options)
