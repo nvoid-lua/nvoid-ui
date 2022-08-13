2c78fdb9a0f7d1c52598cebe807813cf9aac67d1
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-08-14 00:11:21 +0200

    New Theming

--- lua/ui/bufferline/init.lua
+++ lua/ui/bufferline/init.lua
@@ -3,6 +3,7 @@ return {
   run = function()
     if config then
       local M = require "ui.bufferline.modules"
+      require("base16").load_highlight "bufferline"
       return M.CoverNvimTree() .. M.bufferlist() .. (M.tablist() or "") .. M.buttons()
     else
       vim.opt.showtabline = 0
